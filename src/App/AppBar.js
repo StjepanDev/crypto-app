847fac558245171aafb5ce68632f866733491c8b
StjepanDev <grunf69@yahoo.com>
2022-03-05 22:45:53 +0100

    appContext

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { AppContext } from './AppProvider';
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `;
 
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substring(1);
+}
+
+function ControlButton({ name }) {
+  return (
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem onClick={() => setPage(name)} active={page === name}>
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
+}
+
 function AppBar() {
   return (
     <Bar>
-      <div>Crypto</div>
-      <div></div>
-      <div>Dash Board</div>
-      <div>Settings</div>
+      <Logo>Crypto</Logo>
+      <div />
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 }
